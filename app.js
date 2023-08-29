fe1ce04335659eba156332c380e7fe94f02cd213
alvarodavisantos <129607745+alvarodavisantos@users.noreply.github.com>
2023-08-29 12:46:44 -0300

    primeiro comit

---
+++ app.js
@@ -0,0 +1,60 @@
+const projects = [
+    {
+        id: 0,
+        title: "Catálogo de Filmes",
+        description: "A página web apresenta uma coleção de filmes exibidos em uma grade. Os filmes são exibidos em cartões com informações como título, imagem e descrição. Na parte superior ou inferior da grade, existem botões 'Anterior' e 'Próximo' que permitem navegar pelos filmes um por um. Além disso, há um botão 'Aleatório' que direciona o usuário para um filme escolhido aleatoriamente da coleção. Os usuários podem explorar os filmes, visualizar suas informações e escolher aleatoriamente uma opção de entretenimento.",
+        image: "./imgs/project1.png",
+        linkPage: "https://alvarodavisantos.github.io/mudarfilmes/",
+        linkRepository: "https://github.com/alvarodavisantos/mudarfilmes"
+    },
+    {
+        id: 1,
+        title: "Cores aleatórias",
+        description: "A página web apresenta um botão central que, ao ser clicado, muda o plano de fundo da página para uma cor aleatória. Cada vez que o botão é pressionado, uma nova cor vibrante e variada aparece como plano de fundo. Além disso, a página exibe automaticamente o nome e a descrição da cor escolhida, oferecendo uma experiência interativa e visualmente estimulante para os usuários.",
+        image: "./imgs/project2.png",
+        linkPage: "https://alvarodavisantos.github.io/mudarcores/",
+        linkRepository: "https://github.com/alvarodavisantos/mudarcores"
+    },
+    {
+        id: 2,
+        title: "Contador",
+        description: "A página web exibe três botões de ação: um para aumentar, outro para diminuir e um terceiro para zerar um contador. Quando o número no contador é positivo, o botão de aumento fica verde; quando o número é zero, o botão de zerar fica preto; e quando o número é negativo, o botão de diminuir fica vermelho. Essa abordagem visual ajuda os usuários a identificar rapidamente o estado do contador e a executar a ação desejada.",
+        image: "./imgs/project3.png",
+        linkPage: "https://alvarodavisantos.github.io/contador/",
+        linkRepository: "https://github.com/alvarodavisantos/contador"
+    }
+];
+
+numProject = 0;
+const imageProject = document.getElementById('imageProject');
+const descProject = document.getElementById('descProject');
+const titleProject = document.getElementById('titleProject');
+const linkPageProject = document.getElementById('linkPageProject');
+const linkRepositoryProject = document.getElementById('linkRepositoryProject');
+console.log("Valor de imagem: ");
+console.log(imageProject);
+
+function changeProject(){
+    const item = projects[numProject];
+    imageProject.src = item.image;
+    descProject.textContent = item.description;
+    titleProject.textContent = item.title;
+    linkPageProject.href = item.linkPage;
+    linkRepositoryProject.href = item.linkRepository;
+}
+
+function prevProject() {
+    numProject--;
+    if(numProject < 0){
+        numProject = projects.length-1;
+    }
+    changeProject();
+}
+
+function nextProject() {
+    numProject++;
+    if(numProject > projects.length-1){
+        numProject = 0;
+    }
+    changeProject();
+}
